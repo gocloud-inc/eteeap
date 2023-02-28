5dc83c61c8a6107dfee3c97b9485a3b16ffc90e1
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-02-28 16:14:02 +0800

    add turbolinks

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    // Bootstrap Tooltip
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-tooltip="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+document.addEventListener('turbolinks:load', () => {
 
     // Page Loader
     window.addEventListener('beforeunload', (e) => {
         document.body.className = "page-loading";
     }, false);
-
-    // Password Visibility
-    let passwordEl = document.querySelectorAll('.password');
-    let passwordBtn = document.querySelectorAll('.password-btn');
-    let passwordIcon = document.querySelectorAll('.password-icon');
-
-    if (passwordBtn) {
-        passwordBtn.forEach((index) => {
-            index.addEventListener('click', () => {
-                passwordEl.forEach((passwordIndex) => {
-                    if (passwordIndex.type === 'password') {
-                        passwordIndex.type = 'text';
-                        passwordIcon.forEach((passwordIconIndex) => {
-                            passwordIconIndex.innerHTML = 'visibility_off';
-                        });
-                    } else {
-                        passwordIndex.type = 'password';
-                        passwordIcon.forEach((passwordIconIndex) => {
-                            passwordIconIndex.innerHTML = 'visibility';
-                        });
-                    }
-                });
-            });
-        })
-    }
 
     // Clear Form
     let formEl = document.querySelectorAll('.form');
@@ -48,65 +19,39 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Theme Switcher
-    let themeBtnSwitcherEle = document.querySelector('.theme-btn-switcher');
-    let themeIconSwitcher = document.querySelector('.theme-icon-switcher');
-    let themeTextSwitcher = document.querySelector('.theme-text-switcher');
-
-    if (themeBtnSwitcherEle) {
-        themeBtnSwitcherEle.addEventListener('click', () => {
-            document.body.classList.toggle('dark-theme');
-            
-            if (localStorage.getItem('isDarkMode') == 'true') {
-                themeIconSwitcher.innerHTML = 'dark_mode';
-                themeTextSwitcher.innerHTML = 'Dark Mode';
-                localStorage.setItem('isDarkMode', false);
+    let currentStep = 0;
+    let form = document.getElementById("myForm");
+    if (form) {
+        let steps = form.getElementsByClassName("step");
+        let nextButtons = form.getElementsByClassName("next");
+        let prevButtons = form.getElementsByClassName("prev");
+    
+        for (let i = 0; i < nextButtons.length; i++) {
+            nextButtons[i].addEventListener("click", nextStep);
+        }
+    
+        for (let i = 0; i < prevButtons.length; i++) {
+            prevButtons[i].addEventListener("click", prevStep);
+        }
+    
+        function showStep(n) {
+            steps[currentStep].style.display = "none";
+            currentStep = n;
+            steps[currentStep].style.display = "block";
+        }
+    
+        function nextStep() {
+            if (currentStep === steps.length - 1) {
+                form.submit();
             } else {
-                themeIconSwitcher.innerHTML = 'light_mode';
-                themeTextSwitcher.innerHTML = 'Light Mode';
-                localStorage.setItem('isDarkMode', true);
+                showStep(currentStep + 1);
             }
-        });
-
-        if (localStorage.getItem('isDarkMode') == 'true') {     
-            document.body.classList.toggle('dark-theme');
-            themeIconSwitcher.innerHTML = 'light_mode';
-            themeTextSwitcher.innerHTML = 'Light Mode';
         }
-    }
-
-    // Sidebar
-    let sidebarToggler = document.getElementById('sidebar-toggler');
-    let asideEl = document.getElementById('aside');
-
-    if (sidebarToggler) {
-        sidebarToggler.addEventListener('click', () => {
-            asideEl.classList.add('show-aside');
-            asideEl.classList.remove('hide-aside');
-
-            let asideDivOverlay = document.createElement('div');
-        
-            asideDivOverlay.className = 'aside-shadow-overlay'; 
-            document.getElementsByTagName('body')[0].appendChild(asideDivOverlay);
-
-            asideDivOverlay.addEventListener('click', () => {
-                asideEl.classList.add('hide-aside');
-                document.getElementsByTagName('body')[0].removeChild(asideDivOverlay);
-            });
-        
-        });
-    }
-
-    // Sidebar has-children Class
-    function ToggleMenu(event) {
-        event.stopPropagation();
-        if (event.target.firstElementChild) {
-            event.target.classList.toggle('active');
+    
+        function prevStep() {
+            showStep(currentStep - 1);
         }
+    
+        showStep(0);
     }
-
-    let menuParents = document.querySelectorAll(".side-menu .side-item .side-link");
-    menuParents.forEach(menuParent => {
-        menuParent.parentElement.addEventListener("click", ToggleMenu);
-    })
 });
